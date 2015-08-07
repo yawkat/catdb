cbd0a5bf78b96063d99ec71b5a26221f8904a7b6
Jonas Konrad <me@yawk.at>
2015-08-07 21:04:08 +0200

    Fix image modification

--- src/main/java/at/yawk/catdb/db/FileDatabase.java
+++ src/main/java/at/yawk/catdb/db/FileDatabase.java
@@ -93,7 +93,6 @@ class FileDatabase implements Database {
         int maxId = 0;
         for (int i = 0; i < images.size(); i++) {
             Image other = images.get(i);
-            if (other.getUrl().equals(image.getUrl())) { throw new DuplicateImageException(other.getId()); }
             maxId = Math.max(maxId, other.getId());
             if (other.getId() == image.getId()) {
                 if (other.equals(image)) {
@@ -104,6 +103,7 @@ class FileDatabase implements Database {
                 modified = true;
                 break;
             }
+            if (other.getUrl().equals(image.getUrl())) { throw new DuplicateImageException(other.getId()); }
         }
         if (!modified) {
             image.setId(maxId + 1);
